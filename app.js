721d5334672df045562e3358dad98e0746d6981d
StephaTech <stephatech123@gmail.com>
2021-06-17 17:29:32 +0100

    adding style and adding javascript

---
+++ app.js
@@ -0,0 +1,13 @@
+//Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+
+
+//Event Listeners
+function addTodo(event){
+    //Prevent form from submmitting
+}
+
+//Functions
